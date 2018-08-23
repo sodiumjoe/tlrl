3ab545f0f42c952bb57e9eaf553deaefd169d17e
Joe Moon <joe@xoxomoon.com>
2018-08-22 21:24:49 -0700

    start over

--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export const log = (...args) => console.log(...args);
-export const logJson = json => console.log(JSON.stringify(json, null, 2));
-export const error = (...args) => console.error(...args);
